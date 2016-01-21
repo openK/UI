32fac2a3a8cac22d3b02794753c7a8eae6d5fc43
AndreasKuhlmann <Andreas.Kuhlmann@live.de>
2016-01-21 18:00:07 +0100

    fixed transformation state during navigation

--- www/app/app.js
+++ www/app/app.js
@@ -1,4 +1,4 @@
-angular.module('myApp', ['ui.router', 'timer', 'pascalprecht.translate', 'treeGrid', 'isteven-multi-select', 'ui.grid', 'ui.grid.resizeColumns' , 'ui.bootstrap', 'ngResource', 'ui.grid.selection', 'ui.grid.pagination', 'ui.grid.cellNav'])
+angular.module('myApp', ['ui.router', 'timer', 'pascalprecht.translate', 'treeGrid', 'isteven-multi-select', 'ui.grid', 'ui.grid.resizeColumns', 'ui.bootstrap', 'ngResource', 'ui.grid.selection', 'ui.grid.pagination', 'ui.grid.cellNav'])
     .config(['$stateProvider', '$urlRouterProvider', '$translateProvider', '$provide', function ($stateProvider, $urlRouterProvider, $translateProvider, $provide) {
 
         $provide.decorator('$state', [
@@ -54,9 +54,9 @@ angular.module('myApp', ['ui.router', 'timer', 'pascalprecht.translate', 'treeGr
                     activity: function (activityService, $state, $stateParams, $q) {
                         if ($state.current.name === 'state1') {
                             activityService.resetActivity();
-                        }
-                        if ($stateParams.mode !== 'new') {
-                            return activityService.loadActivity();
+                            if ($stateParams.mode !== 'new') {
+                                return activityService.loadActivity();
+                            }
                         }
                         return $q.when();
                     }
@@ -126,151 +126,8 @@ angular.module('myApp', ['ui.router', 'timer', 'pascalprecht.translate', 'treeGr
                 controller: 'CreateProposalConfirmationController',
             })
 
-
-
-
-
-
-            // neue Maßnahme hinzufuegen...
-            .state('ChangeRegulation', {
-                url: "/Change",
-                templateUrl: templPath + "ChangeRegulation.html",
-                controller: 'ChangeRegulationController',
-                abstract: true,
-                resolve: {
-                    activity: function (activityService) {
-                        return activityService.loadConfiguration();
-                    }
-
-                }
-            }).state('ChangeRegulation.ChangeDownRegulation', {
-                url: '/ChangeDownRegulation',
-                templateUrl: templPath + "ChangeDownRegulation.html",
-                controller: 'ChangeDownRegulationController',
-                resolve: {
-                    createActivity: function (activityService) {
-                        return activityService.loadActivity();
-                    }
-                }
-            }).state('ChangeRegulation.NetworkState', {
-                url: '/NetworkState',
-                templateUrl: templPath + "NetworkState.html",
-                abstract: true
-            }).state('ChangeRegulation.NetworkState.Main', {
-                url: '/Main',
-                views: {
-                    "NetworkMainState": {
-                        templateUrl: templPath + "NetworkMainState.html",
-                        controller: 'NetworkMainStateInfoController'
-                    },
-                    "NetworkSubState": {
-                        templateUrl: templPath + "NetworkSubState.html",
-                        controller: 'NetworkSubStateInfoController'
-                    }
-                }
-            }).state('ChangeRegulation.ChangeSettings', {
-                url: '/ChangeSettings',
-                templateUrl: templPath + "ChangeSettings.html",
-                controller: 'CreateSettingsController',
-            }).state('ChangeRegulation.ChangeProposal', {
-                url: '/ChangeProposal',
-                templateUrl: templPath + "ChangeProposal.html",
-                controller: 'ChangeProposalController',
-                abstract: true
-            }).state('ChangeRegulation.ChangeProposal.Main', {
-                url: '/Main',
-                cache: false,
-                views: {
-                    "SelectedNetworkSubStations": {
-                        templateUrl: templPath + "SelectedNetworkSubStation.html",
-                        controller: "SelectedNetworkSubStationController"
-                    },
-                    "NetworkMainState": {
-                        templateUrl: templPath + "NetworkMainState.html",
-                        controller: 'NetworkMainStateController'
-                    },
-                    "NetworkSubState": {
-                        templateUrl: templPath + "NetworkSubState.html",
-                        controller: "NetworkSubStateController"
-                    }
-                }
-            }).state('ChangeRegulation.ChangeProposalConfirmation', {
-                url: '/ChangeProposalConfirmation/',
-                templateUrl: templPath + "ChangeProposalConfirmation.html",
-                controller: 'ChangeProposalConfirmationController',
-            })
-
-
-            // neue Maßnahme bearbeiten...
-            .state('EditRegulation', {
-                url: "/Edit",
-                templateUrl: templPath + "EditRegulation.html",
-                controller: 'EditRegulationController',
-                abstract: true,
-                resolve: {
-                    activity: function (activityService) {
-                        return activityService.loadConfiguration();
-                    }
-
-                }
-            }).state('EditRegulation.EditDownRegulation', {
-                url: '/EditDownRegulation',
-                templateUrl: templPath + "EditDownRegulation.html",
-                controller: 'CreateDownRegulationController',
-                resolve: {
-                    loadActivity: function (activityService) {
-                        return activityService.loadActivity();
-                    }
-                }
-            }).state('EditRegulation.NetworkState', {
-                url: '/NetworkState',
-                templateUrl: templPath + "NetworkState.html",
-                abstract: true
-            }).state('EditRegulation.NetworkState.Main', {
-                url: '/Main',
-                views: {
-                    "NetworkMainState": {
-                        templateUrl: templPath + "NetworkMainState.html",
-                        controller: 'NetworkMainStateInfoController'
-                    },
-                    "NetworkSubState": {
-                        templateUrl: templPath + "NetworkSubState.html",
-                        controller: 'NetworkSubStateInfoController'
-                    }
-                }
-            }).state('EditRegulation.EditSettings', {
-                url: '/EditSettings',
-                templateUrl: templPath + "EditSettings.html",
-                controller: 'CreateSettingsController',
-            }).state('EditRegulation.EditProposal', {
-                url: '/EditProposal',
-                templateUrl: templPath + "EditProposal.html",
-                controller: 'EditProposalController',
-                abstract: true
-            }).state('EditRegulation.EditProposal.Main', {
-                url: '/Main',
-                cache: false,
-                views: {
-                    "SelectedNetworkSubStations": {
-                        templateUrl: templPath + "SelectedNetworkSubStation.html",
-                        controller: "SelectedNetworkSubStationController"
-                    },
-                    "NetworkMainState": {
-                        templateUrl: templPath + "NetworkMainState.html",
-                        controller: 'NetworkMainStateController'
-                    },
-                    "NetworkSubState": {
-                        templateUrl: templPath + "NetworkSubState.html",
-                        controller: "NetworkSubStateController"
-                    }
-                }
-            }).state('EditRegulation.EditProposalConfirmation', {
-                url: '/EditProposalConfirmation/',
-                templateUrl: templPath + "EditProposalConfirmation.html",
-                controller: 'EditProposalConfirmationController',
-            });
         // For any unmatched url, redirect to /state1
-            $urlRouterProvider.otherwise("/Aktiv");
+        $urlRouterProvider.otherwise("/Aktiv");
 
     }
     ]).run(['$log', '$rootScope', '$state', function ($log, $rootScope, $state) {
